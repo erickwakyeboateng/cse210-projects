613fa59094f35de9d01f8c0c465c10ce517d7a91
Eric Boateng <boateng.eric44@gmail.com>
2026-04-14 12:29:26 +0000

    Week07 Exercise Tracking

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("03 Dec 2025", 30, 3.0));
+        activities.Add(new Cycling("18 Jan 2026", 40, 8.9));
+        activities.Add(new Swimming("19 Apr 2026", 20, 17));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
