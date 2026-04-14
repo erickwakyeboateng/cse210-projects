93d414b9f6c32eeca46066e98aec5f49a0111685
Eric Boateng <boateng.eric44@gmail.com>
2026-04-14 12:48:09 +0000

    added creative features comment

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,3 +1,8 @@
+/*
+  Creative Features Added:
+ - Users level up every 1000 points to add motivation beyond scoring.
+ - Goals show clear status indicators ([ ], [X], [∞]) and progress tracking (e.g. 2/5).
+ */
 class Program
 {
     static void Main(string[] args)
